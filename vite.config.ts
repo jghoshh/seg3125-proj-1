0b73c523613ebcf4bdc9ca4a445f429e005cc93e
Jay Ghosh <jayghosh730@gmail.com>
2024-06-08 14:25:24 -0400

    changed vite config to allow env variables to be loaded in at build-time

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const basePath = import.meta.env?.VITE_BASE_PATH || '/';
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base:  basePath
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH
+  };
+});
